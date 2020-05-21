c5b41fae04508cc80ca7e2746474aa960609b4a5
arjavjain59998 <arjavjain59998@gmail.com>
2020-05-21 21:02:55 +0530

    -_-

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 import numpy as np
+import cv2
 import torch
 from torchvision import transforms
+from torch.autograd import Variable
 
 
 
@@ -8,15 +10,18 @@ test_transforms = transforms.Compose([transforms.Resize(224), transforms.ToTenso
 to_pil = transforms.ToPILImage()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model=torch.load('./cricket-model-3.pth')
+
+model=torch.load('./cricket-model-3.pth',map_location={'cuda:0': 'cpu'})
 
 
 
 def predict_image(image):
-	image = to_pil(images)
+	image = to_pil(image)
 	image_tensor = test_transforms(image).float()
 	image_tensor = image_tensor.unsqueeze_(0)
 	input = Variable(image_tensor)
 	output = model(input)
 	index = output.data.cpu().numpy().argmax()
 	return index
+
+
